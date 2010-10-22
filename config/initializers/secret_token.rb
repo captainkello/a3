c252a3701b5a61ee18371305328e4b6d80539a9b
Capt. Kello <captainkello@gmail.com>
2010-10-21 17:09:19 -1000

    Bundle install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+A3::Application.config.secret_token = '4831e0300878c13f71e9f40e2e95b5f0f0e1b9b7d719a78d982d155d0f18762a334b14e889710db4eb095acc5d3d0ed3208cd8efd2ec09289c045cc681f21680'
