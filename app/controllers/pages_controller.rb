26b61408383bf992e4bf98033b28495e6d56187d
Capt. Kello <captainkello@gmail.com>
2010-10-21 18:58:22 -1000

    Basic layout complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,10 @@ class PagesController < ApplicationController
   def about
     @title = "About"
   end
+  
+  def help
+    @title = "Help"
+  end
 
   def home
     @title = "Home"
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Return a title on a per-page basis.
   def title
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Captain Kello's Island"
     if @title.nil?
       base_title
     else
